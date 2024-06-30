28742f61550cb433c10ad52ac7dc175fcd486442
Bence Nagy <nagybence@mailbox.unideb.hu>
2024-06-30 20:09:30 +0200

    Add GET /hello-world

--- src/main/java/org/example/demospring/DemoSpringApplication.java
+++ src/main/java/org/example/demospring/DemoSpringApplication.java
@@ -2,12 +2,25 @@ package org.example.demospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class DemoSpringApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringApplication.class, args);
     }
 
+    @GetMapping("/hello-world")
+    public Map<String, String> helloWorld() {
+        Map<String, String> response = new HashMap<>();
+        response.put("hello", "world");
+        return response;
+    }
 }
